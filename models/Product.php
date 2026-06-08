15314cdac932e63fbedc3344c1fcb2f8526823bc
juju-eiei <yingaung651@gmail.com>
2026-06-08 23:53:02 +0700

    edit data base

--- models/Product.php
+++ models/Product.php
@@ -21,7 +21,7 @@ class Product {
         }
 
         if ($filter === "low_stock") {
-            $conditions[] = "p.stock_qty > 0 AND p.stock_qty < 5";
+            $conditions[] = "p.stock_qty > 0 AND p.stock_qty <= 5";
         } elseif ($filter === "out_of_stock") {
             $conditions[] = "p.stock_qty = 0";
         }
